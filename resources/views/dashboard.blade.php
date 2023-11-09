69a1f0bcbcd9f778d980724f53d1693e432ce79e
masyhudiaffandi <masyhudiaffandi816@gmail.com>
2023-11-09 14:28:46 +0700

    first commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,30 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <form action="/siswa" class="flex flex-col gap-8">
+                        <label for="number">id</label>
+                        <input type="number">
+
+                        <label for="text">nama</label>
+                        <input type="text">
+
+                        <label for="text">alamat</label>
+                        <input type="text" name="" id="">
+
+                        <label for="number">Nomor Telepon</label>
+                        <input type="number" name="" id="">
+
+                        <label for="date">tanggal lahir</label>
+                        <input type="date">
+
+                        <label for="foto">foto</label>
+                        <input type="file" name="" id="">
+
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
+                    </form>
                 </div>
             </div>
         </div>
